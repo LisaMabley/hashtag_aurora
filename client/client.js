6e1e4ffddbe9340701d1d096193f3015e70584a3
LisaMabley <lisamabley@gmail.com>
2016-05-14 19:53:57 -0500

    adds functioning call to twitter

--- client/client.js
+++ client/client.js
@@ -1,14 +1,6 @@
 var app = angular.module('auroraApp', []);
 
 app.controller("IndexController", ['$http', function($http) {
-  console.log('LOADING CONTROLLER');
   var controller = this;
-  controller.start = function() {
-    console.log('START FUNCTION IN CONTROLLER');
-    $http.get('/').then(function(response) {
-      console.log(response.data);
-    })
-  }
 
-  controller.start();
 }]);
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -3,42 +3,57 @@ var path = require('path');
 var OAuth2 = require('oauth').OAuth2;
 var bodyParser = require('body-parser');
 var router = express.Router();
-var Twitter = require('../public/vendor/twitter');
-var base64 = require('../public/vendor/base64');
+var Twitter = require('twitter');
+var base64 = require('base-64');
+var request = require('request');
 
-// var client = new Twitter({
-//   consumer_key: key,
-//   consumer_secret: secret,
-//   bearer_token: token
-// });
+var key = '3lW3QYg9Z8Tyltok8uuYkv01e';
+var secret = '9mS6NKMqa1dtKe8NnAQsRbYtC7WCUDL9OHimr3mSXUSojM6rj0';
+// var token = getBearerToken();
+var token = 'AAAAAAAAAAAAAAAAAAAAALO0vAAAAAAAw5MjA7weaSHVKADP0bMn52mvOKc%3Dc0Migs9DC2VnDI5R3koIyzXRmKUzyDXdQxgKpw6Qd2BPYHLBct';
 
-function getBearerToken() {
-  // var encodedString = base64.encode(key + ':' + secret);
-  // var authRequest = new XMLHttpRequest();
-  // authRequest.open("POST", "https://api.twitter.com/oauth2/token");
-  // authRequest.setRequestHeader("Authorization", "Basic " + encodedString);
-  // authRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-  // authRequest.onreadystatechange = function () {
-      // if (authRequest.readyState == 4) {
-      //     var accessToken = JSON.parse(authRequest.response);
-      //     console.log("access token:", accessToken);
-      // }
-    // }
-    // authRequest.send("grant_type=client_credentials");
-  }
+var client = new Twitter({
+  consumer_key: key,
+  consumer_secret: secret,
+  bearer_token: token
+});
+
+// function getBearerToken() {
+//   var encodedString = base64.encode(key + ':' + secret);
+//
+//   request({
+//       url: 'https://api.twitter.com/oauth2/token',
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//         'Authorization': 'Basic ' + encodedString
+//       },
+//       body: 'grant_type=client_credentials'
+//   }, function(error, response, body){
+//       if(error) {
+//           console.log(error);
+//       } else {
+//           var token = JSON.parse(body).access_token;
+//           console.log(token);
+//           return token;
+//       }
+//   });
+// };
 
 var input = 'climate';
 var hashtag = '#' + input;
 
 // Endpoints
 router.get('/', function(request, response) {
-  console.log('GETTING TWEETS');
-  response.send('Hi!');
-  // Get tweets containing the hashtag
+  response.sendFile(path.join(__dirname, '../public/views/index.html'));
 
-  // client.get('search/tweets', {q: hashtag}, function(error, tweets, response){
-  //   console.log(tweets);
-  // });
+  // Get tweets containing the hashtag
+  client.get('search/tweets', {q: hashtag}, function(error, tweets, response){
+    if(error) {
+      console.log(error);
+    };
+      console.log(tweets);
+  });
 });
 
 // Export
